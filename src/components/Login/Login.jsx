4e6eb936dbdf5f213f4a98b24779ca11b81fdbcf
Silverback2023 <sanctified_rn@att.net>
2023-10-12 23:17:53 -0700

    Add SignUp and Login components

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,14 @@
+// src/components/SignUp/SignUp.jsx
+import React from 'react';
+import './Login.css';
+
+const SignUp = () => {
+  return (
+    <div className="login-container">
+      <h2>Sign Up</h2>
+      {/* Add your sign-up form or content here */}
+    </div>
+  );
+};
+
+export default SignUp;
